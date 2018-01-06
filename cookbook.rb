e17311ad7e1ed3f2aa8e49d5340410146838b544
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-06 14:20:38 +0100

    added csv file and created methods to load and save the cookbook with it

--- cookbook.rb
+++ cookbook.rb
@@ -1,10 +1,13 @@
 class Cookbook
-  def initialize
+  def initialize(csv_file_path)
     @recipes = []
+    @csv_file = csv_file_path
+    CSV.foreach(csv_file_path) { |row| @recipes << Recipe.new(row[0], row[1], row[2]) }
   end
 
   def add(recipe)
     @recipes << recipe
+    save_to_csv
   end
 
   def all
@@ -16,6 +19,17 @@ class Cookbook
   end
 
   def remove(index)
-    @recipes.delete_at[index]
+    @recipes.delete_at(index)
+    save_to_csv
+  end
+
+  private
+
+  def save_to_csv
+    CSV.open(@csv_file, 'wb') do |csv|
+      @recipes.each do |recipe|
+        csv << [recipe]
+      end
+    end
   end
 end
