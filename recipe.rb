4085b40cc4a5132a3ad99e481a66f267d77087b4
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-06 13:51:44 +0100

    created Recipe class and it's initialize with name, description and cooking time

---
+++ recipe.rb
@@ -0,0 +1,9 @@
+class Recipe
+  def initialize(name, description, cooking_time)
+    @name = name
+    @description = description
+    @cooking_time = cooking_time
+  end
+
+  def has_been_tried
+end
