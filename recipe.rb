a14df57d5a7545cc6f7f3c0a191f98bb6da1d3b2
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-08 23:52:47 +0100

    added instance variable to recipe to mark it as done and methods to change the status

--- recipe.rb
+++ recipe.rb
@@ -1,9 +1,18 @@
 class Recipe
   attr_reader :name, :description, :cooking_time
 
-  def initialize(name, description, cooking_time)
+  def initialize(name, description, cooking_time, has_been_done = false)
     @name = name
     @description = description
     @cooking_time = cooking_time
+    @has_been_done = has_been_done
+  end
+
+  def has_been_done?
+    @has_been_done
+  end
+
+  def mark_as_done!
+    @has_been_done = true
   end
 end
