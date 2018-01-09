ee37676e740bb92388c259b7fc859b8c0e792366
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-09 21:20:00 +0100

    created method to scrap recipes descriptions

--- parsing.rb
+++ parsing.rb
@@ -4,7 +4,8 @@ file = 'strawberry.html'
 doc = Nokogiri::HTML(File.open(file), nil, 'utf-8')
 
 titles = doc.xpath("//div[@class='m_titre_resultat']/a")
-# descriptions = doc.xpath("//div[@class='m_detail_recette']")
+descriptions = doc.xpath("//div[@class='m_detail_recette']")
+
 
 
 def get_recipe_title(titles)
@@ -13,11 +14,13 @@ def get_recipe_title(titles)
   end
 end
 
-# def get_recipe_title(titles)
-#   titles.map do |element|
-#     element.text
-#   end
-# end
+def get_recipe_descriptions(descriptions)
+  descriptions.map do |element|
+    element.text.gsub("Recipe -", '').strip
+  end
+end
+
+
 
 
 
