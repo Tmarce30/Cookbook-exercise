9d471fd4c118dc10d660b984613c9eb279836f28
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-10 13:58:55 +0100

    inserted parsing inside a class with a method search that opens an url to scrap it, giving the ability to call it from the controller

--- parsing.rb
+++ parsing.rb
@@ -1,38 +1,22 @@
 require 'nokogiri'
+require 'open-uri'
+require_relative 'recipe'
 
-file = 'strawberry.html'
-html = Nokogiri::HTML(File.open(file), nil, 'utf-8')
+class SearchRecipe
+  def search(ingredient)
+    recipes = []
 
-# titles = doc.xpath("//div[@class='m_titre_resultat']/a")
-# descriptions = doc.xpath("//div[@class='m_detail_recette']")
-# cooking_times = doc.xpath("//div[@class='m_detail_time']/div[@style='float:left;']")
+    url = "http://www.letscookfrench.com/recipes/find-recipe.aspx?type=all&aqt=#{ingredient}"
+    html = Nokogiri::HTML(open(url), nil, 'utf-8')
 
-# def get_recipe_title(titles)
-#   titles.map do |element|
-#     element.text
-#   end
-# end
+    html.search(".recette_classique").each do |doc|
+      name = doc.search(".m_titre_resultat").text.strip
+      description = doc.search(".m_detail_recette").text.gsub("Recipe -", '').strip
+      cooking_time = doc.search(".m_prep_time").first.parent.text.strip
 
-# def get_recipe_descriptions(descriptions)
-#   descriptions.map do |element|
-#     element.text.gsub("Recipe -", '').strip
-#   end
-# end
-
-# def get_recipe_cooking_time(cooking_times)
-#   cooking_times.map do |element|
-#     element.text.first.parent
-#   end
-# end
-
-html.search(".recette_classique").each do |doc|
-  title = doc.search(".m_titre_resultat").text.strip
-  description = doc.search(".m_detail_recette").text.gsub("Recipe -", '').strip
-  cooking_time = doc.search(".m_prep_time").first.parent.text.strip
-  p title
-  p description
-  p cooking_time
+      recipe = Recipe.new(name, description, cooking_time)
+      recipes << recipe
+    end
+    return recipes
+  end
 end
-
-
-
